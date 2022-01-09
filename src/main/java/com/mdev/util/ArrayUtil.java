17f39cff51d28ab36e5fffad4147afdeb39845ae
Munir Zikrullaev <munirprog@gmail.com>
2022-01-09 12:55:55 +0300

    Create util class for generating arrays

---
+++ src/main/java/com/mdev/util/ArrayUtil.java
@@ -0,0 +1,15 @@
+package com.mdev.util;
+
+import java.util.Random;
+
+public class ArrayUtil {
+
+    public static Double[] generateArray(Integer n) {
+        Random ran = new Random();
+        Double[] array = new Double[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = ran.nextDouble();
+        }
+        return array;
+    }
+}
