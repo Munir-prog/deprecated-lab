9974afe4ddcd29c870611861c678ecc97619c15d
Munir Zikrullaev <munirprog@gmail.com>
2022-01-09 12:59:03 +0300

    some changes

--- src/main/java/com/mdev/Application.java
+++ src/main/java/com/mdev/Application.java
@@ -1,26 +1,19 @@
 package com.mdev;
 
-import com.mdev.classes.TestMethods;
-import com.mdev.classes.ThirdClass;
-import com.mdev.config.ConfigurationLog;
 import com.mdev.config.SpringConfiguration;
+import com.mdev.service.BusinessLogic;
+import com.mdev.service.TimerMethods;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
-
     public static void main(String[] args) {
-        var x = new AnnotationConfigApplicationContext(ConfigurationLog.class, SpringConfiguration.class);
-        var bean = x.getBean("smclass", TestMethods.class);
-        bean.doSomething();
-        bean.sheesh();
-        second(x);
+        try (var context = new AnnotationConfigApplicationContext(SpringConfiguration.class)) {
+            var businessLogic = context.getBean("logic", BusinessLogic.class);
+            var timerMethods = context.getBean("timerMethods", TimerMethods.class);
+            businessLogic.sayHello();
+            businessLogic.sayHello10();
+            timerMethods.startMethods();
+        }
     }
-
-    public static void second(AnnotationConfigApplicationContext x) {
-        var bean = x.getBean("tc", ThirdClass.class);
-        bean.entrypoint();
-        x.close();
-    }
-
 }
